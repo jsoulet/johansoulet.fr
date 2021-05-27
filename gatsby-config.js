76732406c0eb44af6994bffec70d153062a8d026
Johan Soulet <johan@troopers.email>
2021-05-28 00:35:45 +0200

    :bug: bring back favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,6 +92,19 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
+      },
+    },
+
+    {
       resolve: `gatsby-plugin-typescript`,
       options: {
         isTSX: true,
